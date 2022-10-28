5db6a8bd4c8e753610d95d2e7f599dc417b2dfed
Apozzz <63206783+Apozzz@users.noreply.github.com>
2022-10-28 10:06:10 +0300

    Added Facade pattern

---
+++ bomberman/classes/facade/CheckPositionValid.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bomberman.classes.facade
+{
+    public class CheckPositionValid : IValidator
+    {
+        int width { get; set; }
+        int height { get; set; }
+        public CheckPositionValid(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public bool IsValid(Player player, Vector2f dirVector)
+        {
+            var nextPosition = Utils.AddVectors(player.Position, dirVector);
+
+            return nextPosition.X >= 0 && nextPosition.Y >= 0 && nextPosition.X <= width - 1 && nextPosition.Y <= height - 1;
+        }
+    }
+}
