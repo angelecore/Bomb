b81e900dc88c6a3a1d0feee1480e3dc1ca014424
Apozzz <63206783+Apozzz@users.noreply.github.com>
2022-11-09 20:55:23 +0200

    dadada

---
+++ Tests/classes/UtilsTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using bomberman.classes;
+using Moq;
+
+namespace Tests.classes
+{
+    public class UtilsTest
+    {
+        [Fact]
+        public void testAddVectorsReturnsNewCorrectVector()
+        {
+            /*var vector2fMockFirst = new Mock<Vector2f>(3, 3);
+            var vector2fMockSecond = new Mock<Vector2f>(3, 7);
+            var utils = new Mock<Utils>();
+            vector2fMockFirst.Setup(x => x.X).Returns(3);
+            vector2fMockFirst.Setup(x => x.Y).Returns(3);
+            vector2fMockSecond.Setup(x => x.X).Returns(3);
+            vector2fMockSecond.Setup(x => x.Y).Returns(7);
+            var vector = new Vector2f(6, 10);
+            Assert.Equal(vector.X, Utils.AddVectors(vector2fMockFirst, vector2fMockSecond);*/
+            var vector1 = new Vector2f(3, 3);
+            var vector2 = new Vector2f(3, 7);
+            var vector3 = new Vector2f(6, 10);
+            Assert.Equal(vector3, Utils.AddVectors(vector1, vector2));
+        }
+        [Fact]
+        public void testMultiplyVectorsReturnsNewCorrectVector()
+        {
+            var vector1 = new Vector2f(3, 7);
+            var vector2 = new Vector2f(9, 21);
+            Assert.Equal(vector2, Utils.MultiplyVector(vector1, 3));
+        }
+
+    }
+}
--- Tests/classes/Vector2fTest.cs
+++ Tests/classes/Vector2fTest.cs
@@ -1,15 +1,33 @@
-﻿using System;
+﻿using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
+using bomberman.classes;
 
 namespace Tests.classes
 {
     public class Vector2fTest
     {
         [Fact]
+        public void testToStringReturnsFormattedData()
+        {
+            var vector2fMock = new Mock<Vector2f>(3, 3);
+            var realVector = new Vector2f(3, 3);
+            //vector2fMock.Setup(x => x.GetType()).Returns(realVector.GetType());
+            Assert.True(realVector.Equals(vector2fMock));
+        }
+
+        [Fact]
+        public void testEqualsReturnsFalse()
+        {
+            var vector2fMock = new Mock<Vector2f>(3, 3);
+            var realVector = new Vector2f(5, 5);
+            //vector2fMock.Setup(x => x.Equals(secondVector2fMock)).Returns(true);
+            Assert.True(realVector.Equals(vector2fMock));
+        }
 
     }
 }
