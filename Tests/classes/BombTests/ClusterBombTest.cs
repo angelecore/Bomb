4d51a0ff78e52eb7829a383806c747b3ddf17927
MisterChoccoMilk <96448964+MisterChoccoMilk@users.noreply.github.com>
2022-11-17 00:03:25 +0200

    tests

---
+++ Tests/classes/BombTests/ClusterBombTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using bomberman.classes;
+using Moq;
+using Xunit.Abstractions;
+
+
+namespace Tests.classes.BombTests
+{
+    public class ClusterBombTest
+    {
+
+        private readonly ITestOutputHelper output;
+
+        public ClusterBombTest(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+        [Fact]
+        public void testClusterBombExplosionPositionsReturn()
+        {
+            Player player = new Player("temp id", "name", new Vector2f(3, 3));
+            player.BombType = BombType.Cluster;
+            Block[,] grid = new Block[5, 5];
+            for (int row = 0; row < 5; row++)
+            {
+                for (int col = 0; col < 5; col++)
+                    grid[row, col] = new Block(new Vector2f(row, col), BlockType.Empty);
+            }
+            Bomb bomb = BombFactory.GetBombInstance(player.BombType, player.Position, player, player.BombExplosionRadius);
+            List<Vector2f> expected = new List<Vector2f>();
+            expected.Add(new Vector2f(3, 3));
+            expected.Add(new Vector2f(4, 3));
+            expected.Add(new Vector2f(2, 3));
+            expected.Add(new Vector2f(3, 4));
+            expected.Add(new Vector2f(3, 2));
+            GameState gameState = new GameState(player.Name, 1);
+            gameState.Width = 5; gameState.Height = 5;
+            var cells = bomb.GetExplosionPositions(grid, (pos) => gameState.IsPositionValid(pos));
+            List<Vector2f> result = new List<Vector2f>();
+            foreach (var cell in cells)
+            {
+                result.Add(cell.Item1);
+            }
+            output.WriteLine("This is output from {0}", result.ToArray());
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ Tests/classes/playerMovementFacadeTest.cs
@@ -0,0 +1,34 @@
+﻿using bomberman.classes;
+using System;
+using Xunit;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using bomberman.classes;
+using System.Drawing.Text;
+using bomberman.classes.facade;
+using System.Numerics;
+
+namespace Tests.classes
+{
+    public class playerMovementFacadeTest
+    {
+        [Fact]
+        public void CanSetDirectionTest()
+        {
+            Player player = new Player("temp id", "name", new Vector2f(4, 4));
+            Block[,] Grid = new Block[5, 5];
+            for (int row = 0; row < 5; row++)
+            {
+                for (int col = 0; col < 5; col++)
+                    Grid[row, col] = new Block(new Vector2f(row, col), BlockType.Empty);
+            }
+            string action = "Up";
+            int Width = 5;
+            int Height = 5;
+            PlayerMovementFacade playerMovementFacade = new PlayerMovementFacade(player, action, Width, Height, Grid);
+            Assert.True(playerMovementFacade.canSetDirection());
+        }
+    }
+}
