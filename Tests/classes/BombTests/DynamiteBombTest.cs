ee2fef9f50c9f8cf4044d86d8b61c3a6b030b9af
angelecore <93863610+angelecore@users.noreply.github.com>
2022-11-16 20:59:14 +0200

    undo 2

--- Tests/classes/BombTests/DynamiteBombTest.cs
+++ Tests/classes/BombTests/DynamiteBombTest.cs
@@ -17,9 +17,7 @@ namespace Tests.classes
                 for (int col = 0; col < 5; col++)
                     grid[row, col] = new Block(new Vector2f(row, col), BlockType.Empty);
             }
-            //Bomb bomb = BombFactory.GetBombInstance(player.BombType, player.Position, player, player.BombExplosionRadius);
-            Bomb bomb = new Bomb(player.Position, player, 2, 0);
-            bomb.setExplosion(BombType.Dynamite);
+            Bomb bomb = BombFactory.GetBombInstance(player.BombType, player.Position, player, player.BombExplosionRadius);
             List<Vector2f> expected = new List<Vector2f>();
             expected.Add(new Vector2f(2, 2));
             expected.Add(new Vector2f(2, 3));
@@ -31,6 +29,7 @@ namespace Tests.classes
             expected.Add(new Vector2f(4, 3));
             expected.Add(new Vector2f(4, 4));
             GameState gameState = new GameState(player.Name, 1);
+            gameState.Width = 5; gameState.Height = 5;
             var cells = bomb.GetExplosionPositions(grid, (pos) => gameState.IsPositionValid(pos));
             List<Vector2f> result = new List<Vector2f>();
             foreach (var cell in cells)
