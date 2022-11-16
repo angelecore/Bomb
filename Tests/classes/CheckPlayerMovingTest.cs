6ce3bcf18cdb7a3f0859f0b7e52293c8149164f1
Apozzz <63206783+Apozzz@users.noreply.github.com>
2022-11-16 23:00:09 +0200

    Added few tests

---
+++ Tests/classes/CheckPlayerMovingTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using bomberman.classes.facade;
+using Moq;
+using bomberman.classes;
+
+namespace Tests.classes
+{
+    public class CheckPlayerMovingTest
+    {
+        [Fact]
+        public void testIsValidReturnsTrueOnIdleDirections()
+        {
+            CheckPlayerMoving checkPlayerMoving = new CheckPlayerMoving();
+            var vector2fMock = new Mock<Vector2f>(3, 3);
+            var playerMock = new Mock<Player>("1", "name", vector2fMock.Object);
+            Assert.True(checkPlayerMoving.IsValid(playerMock.Object, vector2fMock.Object));
+        }
+
+        [Fact]
+        public void testIsValidReturnsFalseOnNonEqualDirections()
+        {
+            CheckPlayerMoving checkPlayerMoving = new CheckPlayerMoving();
+            var vector2fMock = new Mock<Vector2f>(3, 3);
+            var playerMock = new Mock<Player>("1", "name", vector2fMock.Object);
+            playerMock.Object.Direction = Directions.Right;
+            Assert.False(checkPlayerMoving.IsValid(playerMock.Object, vector2fMock.Object));
+        }
+    }
+}
---
+++ Tests/classes/PlayerTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using bomberman.classes;
+using Moq;
+
+namespace Tests.classes
+{
+    public class PlayerTest
+    {
+        [Fact]
+        public void testUpdateTemporaryStatsDoesNotRemoveIfNotFinished()
+        {
+            Player player = new Player("1", "name", new Vector2f(3, 3));
+            PlayerTemporaryStats playerTemporaryStat = new PlayerTemporaryStats();
+            playerTemporaryStat.ActiveTimer = null;
+            playerTemporaryStat.Applied = true;
+            player.TemporaryStats.Add(playerTemporaryStat);
+            var playerStatsCount = player.TemporaryStats.Count;
+            player.UpdateTemporaryStats((float)3.33);
+            Assert.Equal(playerStatsCount, player.TemporaryStats.Count);
+        }
+
+        [Fact]
+        public void testUpdateTemporaryStatsDoesRemoveStatIfFinished()
+        {
+            Player player = new Player("1", "name", new Vector2f(3, 3));
+            PlayerTemporaryStats playerTemporaryStat = new PlayerTemporaryStats();
+            playerTemporaryStat.ActiveTimer = (float)0.8;
+            playerTemporaryStat.Applied = false;
+            player.TemporaryStats.Add(playerTemporaryStat);
+            var playerStatsCount = player.TemporaryStats.Count;
+            player.UpdateTemporaryStats((float)3.33);
+            Assert.NotEqual(playerStatsCount, player.TemporaryStats.Count);
+        }
+    }
+}
